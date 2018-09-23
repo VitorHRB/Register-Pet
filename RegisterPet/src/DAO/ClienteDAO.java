ee4f9820339334fade8b0a17bb87eca26e3166ed
VitorHRB <42875802+VitorHRB@users.noreply.github.com>
2018-09-23 15:49:23 -0300

    Add files via upload
    novas telas

--- RegisterPet/src/DAO/ClienteDAO.java
+++ RegisterPet/src/DAO/ClienteDAO.java
@@ -9,6 +9,12 @@ import java.sql.PreparedStatement;
 import javax.swing.JOptionPane;
 import java.sql.Connection;
 import Model.Cliente;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author Vitor Henrique
@@ -19,7 +25,36 @@ public class ClienteDAO {
     public ClienteDAO(){
         conn = ConnectionDAO.getConnection();
     }
-    
+    public List<Cliente> buscar(String nome) throws Exception {
+
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        List<Cliente> clientes = new ArrayList<>();
+
+        try {
+            stmt = conn.prepareStatement("SELECT * FROM cliente where nome like ?");
+            stmt.setString(1, nome+"%");
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+
+                Cliente cliente = new Cliente();
+             
+                cliente.setNome(rs.getString("nome"));
+                cliente.setEndereco(rs.getString("endereco"));
+                cliente.setTelefone(rs.getString("telefone"));
+                cliente.setEmail(rs.getString("email"));
+                clientes.add(cliente);
+            }
+
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        } 
+        
+        return clientes;
+
+    }
     public void create(Cliente c){
         try {
             PreparedStatement stmt= conn.prepareStatement("INSERT INTO cliente(nome,telefone, endereco, email)"
@@ -35,5 +70,9 @@ public class ClienteDAO {
             JOptionPane.showMessageDialog(null, "Erro ao cadastrar cliente!");
             throw new RuntimeException(ex);
         }
+    }
+
+    public void excluir(String nome, String nome0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
